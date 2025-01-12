343a447d7549d865ceb54e5c3cd9bca5f677ff80
Eduardo Giacometti De Patta <eduardo.depatta@gmail.com>
2025-01-12 19:15:51 -0300

    rm clog

--- src/middlewares/basicAuthMiddleware.ts
+++ src/middlewares/basicAuthMiddleware.ts
@@ -23,15 +23,10 @@ const basicAuthMiddleware: RequestHandler = (req, _res, next) => {
     const [username, password] = credentials.split(':')
 
     if (typeof environments.BASIC_AUTH_USERNAME !== 'string' || typeof environments.BASIC_AUTH_PASSWORD !== 'string') {
-      console.error('Environment variables are not strings:', {
-        username: environments.BASIC_AUTH_USERNAME,
-        password: environments.BASIC_AUTH_PASSWORD
-      })
       throw new HttpException(500, 'Configuração do servidor inválida.')
     }
 
     if (username.trim() === environments.BASIC_AUTH_USERNAME.trim() && password.trim() === environments.BASIC_AUTH_PASSWORD.trim()) {
-      console.log('Authentication successful')
       return next()
     }
 
